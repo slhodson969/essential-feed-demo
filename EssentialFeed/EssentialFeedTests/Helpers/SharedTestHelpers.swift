246908c99b5e90be81eb304071db8e9ee38c3ab3
Scott Hodson <scott@keepitswift.com>
2022-10-04 15:57:04 +0200

    Implement `URLSessionHTTPClient`

--- EssentialFeed/EssentialFeedTests/Helpers/SharedTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
+
 func anyURL() -> URL {
     return URL(string: "http://any-url.com")!
 }
---
+++ EssentialFeed/EssentialFeedTests/Helpers/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,16 @@
+//
+//  XCTestCase+MemoryLeakTracking.swift
+//  EssentialFeedTests
+//
+//  Created by Scott Hodson on 04/10/2022.
+//
+
+import XCTest
+
+extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+}
