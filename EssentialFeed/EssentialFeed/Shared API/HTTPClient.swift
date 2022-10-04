8c1b5c5e1d431493f9883e48f68b08abb0625454
Scott Hodson <scott@keepitswift.com>
2022-10-04 15:49:56 +0200

    Add `HTTPClient` protocol boundary

---
+++ EssentialFeed/EssentialFeed/Shared API/HTTPClient.swift
@@ -0,0 +1,21 @@
+//
+//  HTTPClient.swift
+//  EssentialFeed
+//
+//  Created by Scott Hodson on 04/10/2022.
+//
+
+import Foundation
+
+public protocol HTTPClientTask {
+    func cancel()
+}
+
+public protocol HTTPClient {
+    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
+    @discardableResult
+    func get(from url: URL, completion: @escaping (Result) -> Void) -> HTTPClientTask
+}
