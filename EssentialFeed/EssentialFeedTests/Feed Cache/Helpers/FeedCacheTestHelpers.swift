fe659159f6f7aa2ef7bca697cfd8a82e85b09415
Scott Hodson <scott@keepitswift.com>
2022-10-05 16:48:21 +0200

    Ensure that load delivers cached images on non expired cache

--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -17,3 +17,13 @@ func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
     let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
     return (models, local)
 }
+
+extension Date {
+    func minusFeedCacheMaxAge() -> Date {
+        return adding(days: -feedCacheMaxAgeInDays)
+    }
+    
+    private var feedCacheMaxAgeInDays: Int {
+        return 7
+    }
+}
