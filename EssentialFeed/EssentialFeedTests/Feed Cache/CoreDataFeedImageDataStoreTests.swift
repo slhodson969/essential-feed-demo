0fd33bb361b9eef58f83e8096e6d7df3526a3899
Scott Hodson <scott@keepitswift.com>
2022-10-07 16:43:58 +0200

    Ensure that retrieveImageData delivers found data when there is a stored image data matching URL

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -26,6 +26,16 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
         expect(sut, toCompleteRetrievalWith: notFound(), for: nonMatchingURL)
     }
     
+    func test_retrieveImageData_deliversFoundDataWhenThereIsAStoredImageDataMatchingURL() {
+        let sut = makeSUT()
+        let storedData = anyData()
+        let matchingURL = URL(string: "http://a-url.com")!
+        
+        insert(storedData, for: matchingURL, into: sut)
+        
+        expect(sut, toCompleteRetrievalWith: found(storedData), for: matchingURL)
+    }
+    
     // - MARK: Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CoreDataFeedStore {
@@ -37,6 +47,10 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
     
     private func notFound() -> Result<Data?, Error> {
         return .success(.none)
+    }
+    
+    private func found(_ data: Data) -> Result<Data?, Error> {
+        return .success(data)
     }
     
     private func localImage(url: URL) -> LocalFeedImage {
