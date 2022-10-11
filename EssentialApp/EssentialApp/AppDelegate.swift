3063c7f6ee1c1b98332991c55a2efcb75d5fe38f
Scott Hodson <scott@keepitswift.com>
2022-10-11 16:29:42 +0200

    Create `EssentialApp` project

---
+++ EssentialApp/EssentialApp/AppDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  AppDelegate.swift
+//  EssentialApp
+//
+//  Created by Scott Hodson on 11/10/2022.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {}
