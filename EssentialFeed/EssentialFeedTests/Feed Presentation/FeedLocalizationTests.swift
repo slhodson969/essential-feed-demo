b41712f73f4e06e289d1cd9595d3ae00a4390b16
Scott Hodson <scott@keepitswift.com>
2022-10-11 17:03:30 +0200

    Ensure that translated values exist for all supported languages

---
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedLocalizationTests.swift
@@ -0,0 +1,20 @@
+//
+//  FeedLocalizationTests.swift
+//  EssentialFeedTests
+//
+//  Created by Scott Hodson on 11/10/2022.
+//
+
+import XCTest
+import EssentialFeed
+
+final class FeedLocalizationTests: XCTestCase {
+    
+    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+        let table = "Feed"
+        let bundle = Bundle(for: FeedPresenter.self)
+        
+        assertLocalizedKeyAndValuesExist(in: bundle, table)
+    }
+    
+}
