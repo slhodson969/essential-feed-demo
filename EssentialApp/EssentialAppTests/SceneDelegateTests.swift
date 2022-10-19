5a5992238b36c27e5fab77bda24fddb100573c7a
Scott Hodson <scott@keepitswift.com>
2022-10-19 12:19:31 +0200

    Ensure that configure window configures root view controller

--- EssentialApp/EssentialAppTests/SceneDelegateTests.swift
+++ EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -21,6 +21,20 @@ class SceneDelegateTests: XCTestCase {
         XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1, "Expected to make window key and visible")
     }
     
+    func test_configureWindow_configuresRootViewController() {
+        let sut = SceneDelegate()
+        sut.window = UIWindowSpy()
+        
+        sut.configureWindow()
+        
+        let root = sut.window?.rootViewController
+        let rootNavigation = root as? UINavigationController
+        let topController = rootNavigation?.topViewController
+        
+        XCTAssertNotNil(rootNavigation, "Expected a navigation controller as root, got \(String(describing: root)) instead")
+        XCTAssertTrue(topController is ListViewController, "Expected a feed controller as top view controller, got \(String(describing: topController)) instead")
+    }
+    
     private class UIWindowSpy: UIWindow {
         var makeKeyAndVisibleCallCount = 0
         
